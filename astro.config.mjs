fc544fee6cb448bc2c147d89dcc0dcadf6e86e9a
Michael Cabrera <mike@usrad.com>
2025-04-23 16:46:02 -0400

    Save work on login pages and updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+// This import should be for @astrojs/tailwind
+import tailwindcss from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwindcss()],
 });
