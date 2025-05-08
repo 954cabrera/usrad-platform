f034ba24786cff3ae2094ba7123d704e958ee284
Michael Cabrera <mike@usrad.com>
2025-05-08 18:48:26 -0400

    added news and careers pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: 'server',  // Enable SSR
+  adapter: node({
+    mode: 'standalone' // Creates a standalone server
+  }),
 });
