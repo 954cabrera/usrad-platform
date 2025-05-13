92cccb90f56cb051a80619f9ede771fc42e9cfe1
Michael Cabrera <mike@usrad.com>
2025-05-12 20:31:34 -0400

    resolved dark issue on chrome

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
       fontFamily: {
         manrope: ['Manrope', 'sans-serif'],
       },
+      colors: {
+        primary: '#003087',
+        accent: '#cc9933',
+        background: '#fcf9f0',
+        backgroundAlt: '#f5e7c5',
+      },
     },
   },
   plugins: [],
