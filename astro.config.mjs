faa05a57e683e491661e422f8dc9eb9da4a17e2b
Michael Cabrera <mike@usrad.com>
2025-05-24 14:05:53 -0400

    optimized mobile for index home and about pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// Simplified configuration for static deployment
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'static'
+  output: 'static',
+  server: {
+    host: true,       // <-- allows access via LAN IP
+    port: 4321        // <-- optional, but useful to fix the port
+  }
 });
