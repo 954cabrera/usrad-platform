188df76bad02f6b15eea829e796e2fdd40d16935
Michael Cabrera <github.x1@macworld.cc>
2025-08-14 20:02:23 -0400

    revised conf.astro

--- public/scripts/providers/confirmation/modules/index.js
+++
@@ -1,8 +0,0 @@
-// modules/index.js - Export all confirmation page modules
-
-export { OrganizationLoader } from './organizationLoader.js';  // lowercase 'o'
-export { CentersLoader } from './centersLoader.js';  // lowercase 'c'
-export { PricingLoader } from './pricingLoader.js';  // lowercase 'p'
-export { ChecklistManager } from './checklistManager.js';  // lowercase 'c'
-export { NavigationManager } from './navigationManager.js';  // lowercase 'n'
-export { ExhibitBManager } from './exhibitBManager.js';  // lowercase 'e'
